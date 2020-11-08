5db44b25611b24b727110712a4998dd3385a5677
Javier <Javier@192.168.56.1>
2020-11-08 03:58:00 +0100

    Escrito hola mundo

--- PrimeraAplicación/src/primeraaplicación/SegundaClase.java
+++ PrimeraAplicación/src/primeraaplicación/SegundaClase.java
@@ -9,13 +9,6 @@ package primeraaplicación;
  *
  * @author Javier
  */
-public class PrimeraAplicación {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
+public class SegundaClase {
     
 }
